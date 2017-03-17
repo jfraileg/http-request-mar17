49d1ff4b8e93ec352f1d9c570dee1dd0c28c7dc4
jfraileg <jfraileg@everis.com>
2017-03-17 13:03:10 +0100

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
-   appId           : '29172a9c-c893-456a-bc8c-648132e35390',
-   clientId        : 'rsuy7t23gnftgl6kr7lov4ow',
-   clientSecret    : 'Rr7ic191JGyKXfjA5srWU8XX',
-   appSignature    : 'hoi2cjh1liqcvr1lvqhj5w2ru44eguauia23pcqf0ujimstmetxodra1ai24vnikeb5fcq1vtdgsxflw5dqx3ijqpt0ocwo5o3nhomgxogimkqbgbn5etfp5agvx4n5dmtk3zaww3woz3jrfehceyivlpcc2cc5z3ytvkpdz3hrbkjunn5ninekevhgpbrygn5r3w4mlzabr2r0hnvxcmv0avr0fepercruu0zqkaw55da21wtxzifyblaxn15b',
+   appId           : 'c54532c6-5c61-4d35-bdde-072176b1b0c2',
+   clientId        : 'lvdw7bluex07xlhm8o9x5bih',
+   clientSecret    : '87k1zbrKt0Mr977L58lCDbuw',
+   appSignature    : 'xstphoytbieh45jzyczxofmvkeks4tpvdq1nmw5qw2ga1v0md4ykler5yryftylsekpm25kwrqrcs15dxwhipdoaoutm3cik4jqxuccwmgdihfn2xt0rartqvkxjs30sbp0r04hnwpbdib3gnjjdgooz3uqmyozapvuhljyutpyqxmgy2nq0ix33ydzmrffaw0sjsqfj15kqeyekeh04adlqke4j54ey1mdaxqzqyoifbii1l0fq3e0bc35lpf2',
    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
